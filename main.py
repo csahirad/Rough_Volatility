ff5ab0a4c80d3ecdf7500fffc5bb448f53efd5c2
csahirad <csahirad@gmail.com>
2021-03-14 17:10:42 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,34 +1,38 @@
 from RBergomi import RBergomi
 from VolSurface import VolSurface
+from Theta import *
 from utils import bsinv
 from payoffs import *
-
+import matplotlib.pyplot as plt
 
 vec_bsinv = np.vectorize(bsinv)
 k = np.arange(-0.5, 0.51, 0.01)
 K = np.exp(k)[np.newaxis, :]
-Ts = np.arange(0.1, 2.0, 0.1)[:, np.newaxis]
-implied_vols = np.zeros((Ts.shape[0], K.shape[1]))
-i = 0
+Ts = np.array([0.3, 0.6, 0.9, 1.2, 1.5, 1.8, 2.0])[:, np.newaxis]
+Tmax = np.max(Ts)
 np.random.seed(0)
-for T in Ts:
-    rB = RBergomi(xis=[(0.0, 0.235**2), (0.25, 0.235**2), (0.5, 0.235**2), (0.75, 0.235**2), (2.0, 0.235**2)],
-                  n=100,
-                  N=30000,
-                  T=T,
-                  a=-0.43,
-                  eta=1.9,
-                  rho=-0.9)
-    dW1 = rB.dW1()
-    dW2 = rB.dW2()
-    Y = rB.Y(dW1)
-    dB = rB.dB(dW1, dW2)
-    XI = rB.XI(interp_method='pwc')
-    V = rB.V(Y, XI)
-    S = rB.S(V, dB)
-    call_prices = vanilla(S, K)
-    implied_vols[i, :] = np.squeeze(vec_bsinv(call_prices, 1., np.transpose(K), rB.T))
-    i += 1
+theta = Theta(xi_ub=0.16, xi_lb=0.01, eta_ub=4.0, eta_lb=0.5, rho_ub=-0.1, rho_lb=-0.95, alpha_ub=0, alpha_lb=-0.475)
+rB = RBergomi(xis=[(0.0, theta.xi0), (0.25, theta.xi1), (0.5, theta.xi2), (0.75, theta.xi3), (1.0, theta.xi4),
+                   (1.25, theta.xi5), (1.5, theta.xi6), (1.75, theta.xi7), (2.0, theta.xi8)],
+              n=100,
+              N=30000,
+              T=Tmax,
+              a=theta.alpha,
+              eta=theta.eta,
+              rho=theta.rho)
+dW1 = rB.dW1()
+dW2 = rB.dW2()
+Y = rB.Y(dW1)
+dB = rB.dB(dW1, dW2)
+XI = rB.XI(interp_method='pwc')
+V = rB.V(Y, XI)
+S = rB.S(V, dB)
+implied_vols = np.zeros((Ts.shape[0], K.shape[1]))
+
+for idx, t in enumerate(Ts):
+    call_prices = vanilla(S[:, :int(t*rB.n) + 1], K)
+    implied_vols[idx, :] = np.squeeze(vec_bsinv(call_prices, 1., np.transpose(K), t))
 
 VS = VolSurface(k, Ts, implied_vols)
-VS.plot_smile(T=1.0)
+VS.plot_surface()
+
